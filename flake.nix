97dc100c97891f5d5e264e6886c8d450f909b08d
Nick Mazuk <Nick-Mazuk@users.noreply.github.com>
2024-10-18 03:54:40 -0700

    Add initial nix configuration

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Breakout";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils }:
+    let
+      name = "breakout";
+    in
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        lib = pkgs.lib;
+      in
+      {
+        devShell = pkgs.mkShell {
+          name = name;
+          buildInputs = with pkgs; [
+            odin
+          ];
+        };
+      }
+    );
+}
